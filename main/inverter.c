13ad07e2c3dd215665d09be4ba7ac26507d530f7
arturmagnus <artursungam@gmail.com>
2023-02-22 18:29:50 -0300

    Arruma descontinuidade na tabela de senos

--- main/inverter.c
+++ main/inverter.c
@@ -54,6 +54,9 @@
 #define PWM_WH_GPIO 17
 #define PWM_WL_GPIO 16
 
+//Pin connected to ESP32 datalogger, 1 means Tjunction overtemperature
+#define TEMP_FAULT_GPIO 23
+
 #define MCPWM_OP_INDEX_U 0
 #define MCPWM_OP_INDEX_V 1
 #define MCPWM_OP_INDEX_W 2
@@ -72,11 +75,10 @@ static void mcpwm_update_TIMER_CMPA(void *args)
     // Once each UPDATE_LOOP_PRD_US we increase the index of a modulated sine-wave. Because we have 111 points, for a 60hz sinewave, the UPDATE_PRD should be
     // 1/111*60, wich is the calculated by the initial defintion.
     index = (index == 111) ? 0 : (index + 1);
-
-    // PIADA DO PROGRAMADOR ENTRRANDO NO BAR!!! 012
-    // TIMER_CMPA[0] = 10 * sine_tableA[index]; // Multiplied by 10 because of the timer resolution. The modulated wave is compited for a 1us res.PRD
-    // TIMER_CMPA[1] = 10 * sine_tableB[index]; // so, the TIMER_PRD max is 555, an thus, the max of sinusoidal wave is TIMER_PRD/2. As we increase the
-    // TIMER_CMPA[2] = 10 * sine_tableC[index]; // resolution, each tick counts 0.1us instead of 1us, so, TIMER_PRD become 5550, and if we keep same peak value for
+    
+    //TIMER_CMPA[0] = 10 * sine_tableA[index]; // Multiplied by 10 because of the timer resolution. The modulated wave is compited for a 1us res.PRD
+    //TIMER_CMPA[1] = 10 * sine_tableB[index]; // so, the TIMER_PRD max is 555, an thus, the max of sinusoidal wave is TIMER_PRD/2. As we increase the
+    //TIMER_CMPA[2] = 10 * sine_tableC[index]; // resolution, each tick counts 0.1us instead of 1us, so, TIMER_PRD become 5550, and if we keep same peak value for
                                                 //  the modulated sinusoid the result isn't right.
 
     TIMER_CMPA[0] = (int)(K_MOD * sine_tableA[index]);
@@ -127,6 +129,26 @@ void app_main(void)
         ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(comparators[i], 100));
     }
 
+    ESP_LOGI(TAG, "Create over current fault detector");
+    mcpwm_fault_handle_t over_cur_fault = NULL;
+    mcpwm_gpio_fault_config_t gpio_fault_config = {
+        .gpio_num = TEMP_FAULT_GPIO,
+        .group_id = 0,
+        .flags.active_level = 1, // low level means fault, refer to DRV8302 datasheet
+        .flags.pull_up = false,   // internally pull up
+    };
+    ESP_ERROR_CHECK(mcpwm_new_gpio_fault(&gpio_fault_config, &over_cur_fault));
+
+    ESP_LOGI(TAG, "Set brake mode on the fault event");
+    mcpwm_brake_config_t brake_config = {
+        .brake_mode = MCPWM_OPER_BRAKE_MODE_CBC,
+        .fault = over_cur_fault,
+        .flags.cbc_recover_on_tez = true,
+    };
+    for (int i = 0; i < 3; i++) {
+        ESP_ERROR_CHECK(mcpwm_operator_set_brake_on_fault(operators[i], &brake_config));
+    }
+
     ESP_LOGI(TAG, "Create PWM generators");
     mcpwm_gen_handle_t generators[3][2] = {};
     mcpwm_generator_config_t gen_config = {};
@@ -156,6 +178,14 @@ void app_main(void)
         ESP_ERROR_CHECK(mcpwm_generator_set_actions_on_compare_event(generators[i][MCPWM_GEN_INDEX_HIGH],
                                                                      MCPWM_GEN_COMPARE_EVENT_ACTION(MCPWM_TIMER_DIRECTION_DOWN, comparators[i], MCPWM_GEN_ACTION_LOW),
                                                                      MCPWM_GEN_COMPARE_EVENT_ACTION_END()));
+
+        ESP_ERROR_CHECK(mcpwm_generator_set_actions_on_brake_event(generators[i][MCPWM_GEN_INDEX_HIGH],
+                                                                     MCPWM_GEN_BRAKE_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, MCPWM_OPER_BRAKE_MODE_CBC, MCPWM_GEN_ACTION_LOW),
+                                                                     MCPWM_GEN_BRAKE_EVENT_ACTION_END()));
+        ESP_ERROR_CHECK(mcpwm_generator_set_actions_on_brake_event(generators[i][MCPWM_GEN_INDEX_HIGH],
+                                                                     MCPWM_GEN_BRAKE_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, MCPWM_OPER_BRAKE_MODE_CBC, MCPWM_GEN_ACTION_LOW),
+                                                                     MCPWM_GEN_BRAKE_EVENT_ACTION_END()));
+    
     }
 
     ESP_LOGI(TAG, "Setup deadtime: %i ns", CUSTOM_DEADTIME * 100);
