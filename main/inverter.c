ab4ec1050384e5d828a84c53ed527eaa9c62870c
arturmagnus <artursungam@gmail.com>
2023-02-25 10:43:46 -0300

    Remove trava de segurança pela GPIO22

--- main/inverter.c
+++ main/inverter.c
@@ -6,6 +6,7 @@
    software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
    CONDITIONS OF ANY KIND, either express or implied.
 */
+
 #include <math.h>
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
